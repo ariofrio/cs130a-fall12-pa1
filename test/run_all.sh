26db65475e47e89577132e09b79cccb7d2df294a
Andres Riofrio <riofrios@gmail.com>
2012-10-30 01:36:04 -0700

    Fail when any of the tests fail

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 TESTDIR=$(dirname $0)
+status=0
 
 for i in $TESTDIR/*.in; do
   o=${i%.in}.out
   echo -n $(tput bold)
   printf "%-37s   %s\n" $o "./quash < $i"
   echo -n $(tput sgr0)
-  ./quash < $i | sdiff -w80 $o -
+  ./quash < $i | sdiff -w80 $o - || status=1
 done
+
+exit $status
